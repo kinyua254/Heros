b80e81a246d10d016bd377b82922a0b41200344d
kinyua254 <victor.mutugi@student.moringaschool.com>
2022-03-02 15:38:57 +0300

    adding logic to the hero and squad classes

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,56 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Hero {
+    private String heroName;
+    private String heroAge;
+    private String heroPower;
+    private String heroWeakness;
+
+    private static ArrayList<Hero> listOfHeroes = new ArrayList<Hero>();
+    private int id;
+
+    public Hero(String heroName, String heroAge, String heroPower, String heroWeakness) {
+        this.heroName = heroName;
+        this.heroAge = heroAge;
+        this.heroPower = heroPower;
+        this.heroWeakness = heroWeakness;
+        listOfHeroes.add(this);
+        this.id = listOfHeroes.size();
+    }
+
+
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public String getHeroAge() {
+        return heroAge;
+    }
+
+    public String getHeroPower() {
+        return heroPower;
+    }
+
+    public String getHeroWeakness() {
+        return heroWeakness;
+    }
+
+    public static ArrayList<Hero> getListOfHeroes() {
+        return listOfHeroes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static Hero findById(int id){
+        return listOfHeroes.get(id - 1);
+    }
+
+    public static void cleaerAllPosts(){
+        listOfHeroes.clear();
+    }
 }
