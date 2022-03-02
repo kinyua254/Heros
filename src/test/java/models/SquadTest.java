9b7b73d6549c328658b2448fd77a0472970ee61a
kinyua254 <victor.mutugi@student.moringaschool.com>
2022-03-02 15:37:19 +0300

    adding logic to the tests and the app.java classes

--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -1,4 +1,60 @@
 package models;
 
-public class SquadTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SquadTest {
+
+    @BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void SquadInstantiatesCorrectly() throws Exception {
+        Squad squad = new Squad("Avengers", "10", "Save America");
+        assertEquals(true, squad instanceof Squad);
+    }
+
+    @Test
+    public void Squad_getsSquadNameCorrectly_string() throws Exception{
+        Squad squad = new Squad("Avengers", "10", "Save America");
+        assertEquals("Avengers", squad.getSquadName());
+    }
+
+    @Test
+    public void Squad_getsMaxSizeCorrectly_int() throws Exception{
+        Squad squad = new Squad("Avengers", "10", "Save America");
+        assertEquals("10", squad.getSquadSize());
+    }
+
+    @Test
+    public void Squad_getsSquadsNameCorrectly() throws Exception {
+        Squad squad = new Squad("Avengers", "10", "Save America");
+        assertEquals("Save America", squad.getSquadCause());
+    }
+
+    @Test
+    public void all_returnsAllInstancesOfSquad_true() throws Exception {
+        Squad firstSquad = new Squad("Avengers", "10", "Save America");
+        Squad secondSquad = new Squad("SuperMan", "15", "fight-corruption");
+        assertTrue(Squad.getListOfSquads().contains(firstSquad));
+        assertTrue(Squad.getListOfSquads().contains(secondSquad));
+    }
+
+    @Test
+    public void find_returnsSquadWithSameId_secondSquad() {
+        Squad firstSquad = new Squad("Avengers", "10", "Save America");
+        Squad secondSquad = new Squad("SuperMan", "15", "fight-corruption");
+        assertEquals(Squad.find(secondSquad.getId()), secondSquad);
+    }
+
+
 }
+
